2581bc45acde6ea3bf6248afc5824fdcf1816056
Udith_Gayan <udith.indrakantha@gmail.com>
2023-03-05 23:01:30 +0530

    Added custom error messages.

--- CM.Middleware/GlobalErrorHandler.cs
+++ CM.Middleware/GlobalErrorHandler.cs
@@ -26,27 +26,32 @@ namespace CM.Middleware
             {
                 var response = context.Response;
                 response.ContentType = "application/json";
+                string errorMessage = null;
 
                 switch (error)
                 {
                     case CustomException e:
                         response.StatusCode = (int)HttpStatusCode.BadRequest;
+                        errorMessage = "Bad Request";
                         break;
                     case KeyNotFoundException e:
                         response.StatusCode = (int)HttpStatusCode.NotFound;
+                        errorMessage = "Not Found";
                         break;
                     case UnauthorizedAccessException e:
                         response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                        errorMessage = "Unauthorized";
                         break;
                     default:
                         response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        errorMessage = "Server Error";
                         break;
                 }
 #if DEBUG
                 var result = JsonSerializer.Serialize(new { message = error?.Message });
                 await response.WriteAsync(result);
 #else
-                  var result = JsonSerializer.Serialize(new { message = error?.Message });
+                var result = JsonSerializer.Serialize(new { message = errorMessage });
                 await response.WriteAsync(result);
 #endif
             }
